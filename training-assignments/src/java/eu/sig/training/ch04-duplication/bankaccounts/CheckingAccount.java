b5db126c725b38c67c80d4ad4f1302b901075194
daniquemont <44334393+daniquemont@users.noreply.github.com>
2024-06-28 14:06:48 +0200

    functies verwijderd die niet nodig zijn

--- training-assignments/src/java/eu/sig/training/ch04-duplication/bankaccounts/CheckingAccount.java
+++ training-assignments/src/java/eu/sig/training/ch04-duplication/bankaccounts/CheckingAccount.java
@@ -2,17 +2,10 @@ package eu.sig.training.ch04;
 
 // tag::CheckingAccount[]
 public class CheckingAccount {
-    private static final float INTEREST_PERCENTAGE = 0.01f;
     private Money balance = new Money();
-    private int transferLimit = 100;
 
     public Transfer makeTransfer(String counterAccount, Money amount)
-        throws BusinessException {
-        // 1. Check withdrawal limit:
-        if (amount.greaterThan(this.transferLimit)) {
-            throw new BusinessException("Limit exceeded!");
-        }
-        // 2. Assuming result is 9-digit bank account number, validate 11-test:
+        throws BusinessException {     
         int sum = 0;
         for (int i = 0; i < counterAccount.length(); i++) {
             char character = counterAccount.charAt(i);
@@ -26,15 +19,6 @@ public class CheckingAccount {
             return result;
         } else {
             throw new BusinessException("Invalid account number!");
-        }
-    }
-
-    public void addInterest() {
-        Money interest = balance.multiply(INTEREST_PERCENTAGE);
-        if (interest.greaterThan(0)) {
-            balance.add(interest);
-        } else {
-            balance.substract(interest);
         }
     }
 }
